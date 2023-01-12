196a16812d5fa6e09fa47a08cfd18371dd2a0814
joseamartinez44 <josep.castelan@gmail.com>
2023-01-11 23:48:24 -0600

    Configuración de datasource con properties y classes

--- src/main/java/com/sm/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/sm/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.sm.fundamentos.bean.MyBeanWithPropertiesImplement;
 import com.sm.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // https://tuhrig.de/using-configurationproperties-to-separate-service-and-configuration/
 
@@ -25,5 +28,15 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithProperties beanWithProperties() {
         return new MyBeanWithPropertiesImplement(name, lastname);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 }
