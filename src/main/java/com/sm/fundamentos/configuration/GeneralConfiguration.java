30cc83ef42121a4a53b381917d5592e5c7e278bd
joseamartinez44 <josep.castelan@gmail.com>
2023-01-15 01:13:25 -0600

    Uso de anotación value para apuntar a properties

--- src/main/java/com/sm/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/sm/fundamentos/configuration/GeneralConfiguration.java
@@ -8,35 +8,55 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 // https://tuhrig.de/using-configurationproperties-to-separate-service-and-configuration/
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
-
     @Value("${value.lastname}")
     private String lastname;
 
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${driver}")
+    private String driver;
+    @Value("${username}")
+    private String userName;
+    @Value("${password}")
+    private String password;
+
     @Bean
     public MyBeanWithProperties beanWithProperties() {
         return new MyBeanWithPropertiesImplement(name, lastname);
     }
 
-    @Bean
+    /*@Bean
     public DataSource dataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.h2.Driver");
         dataSourceBuilder.url("jdbc:h2:mem:testdb");
         dataSourceBuilder.username("sa");
         dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }*/
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(userName);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
